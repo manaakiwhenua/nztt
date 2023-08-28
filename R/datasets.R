3794ff250545c41e84ae42f8cd36f6a65b6c4335
Pierre Roudier <pierre.roudier@gmail.com>
2023-08-28 14:10:43 +1200

    moved dataset docs to datasets.R

---
+++ R/datasets.R
@@ -0,0 +1,31 @@
+#' New Zealand Soil Texture Triangle
+#'
+#' New Zealand Soil Texture Classification Data
+#'
+#' @format `nztt`
+#' A data frame with 42 rows and 5 columns:
+#' \describe{
+#'   \item{clay, sand, silt}{Percentages for clay, sand, and silt}
+#'   \item{name}{Name of the texture classes}
+#'   \item{code}{Code for the texture classes}
+#' }
+#' @source Milne, J. D. G., B. Clayden, P. L. Singleton, and A. D. Wilson. 1995. Soil Description Handbook. Revised. Lincoln, N.Z.: Manaaki Whenua Press.
+#' @author Lauren O'Brien
+"nztt"
+
+#' Soil Texture Data from the Wairau Valley
+#'
+#' Soil Texture Data from the Wairau Valley
+#'
+#' @format `wairau`
+#' A data frame with 10 rows and 6 columns:
+#' \describe{
+#'   \item{id}{A unique sample identifier}
+#'   \item{clay, sand, silt}{Percentages for clay, sand, and silt}
+#'   \item{name}{Name of the texture classes}
+#'   \item{code}{Code for the texture classes}
+#' }
+#' @source Milne, J. D. G., B. Clayden, P. L. Singleton, and A. D. Wilson. 1995. Soil Description Handbook. Revised. Lincoln, N.Z.: Manaaki Whenua Press.
+#' @author Kirstin Deuss
+"wairau"
+
